594c8a857da3ae0e7b7b8caac5048ca394470d60
jenmlucas <jenmlucas1@gmail.com>
2021-10-12 12:05:37 -0700

    text saves to localStorage, colors of calender change with current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var saveBtn= $(".btn");
+var saveBtn = $(".btn");
 
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
@@ -19,31 +19,46 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours-- requirement--- Done in HTML
 
-
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-
-
-
-
 // WHEN I click into a time block
 // THEN I can enter an event
 
-$(".description").on("click", "p", function(){
-  var textInfo = $(this).text().trim();
-  
-  console.log(textInfo);
-
-  saveTasks();
-})
-
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
-var saveTasks = function(){
- textInfo= localStorage.setItem("textInfo", JSON.stringify(this));
+var saveTasks = function () {
+  var text = $(this).siblings(".description").val();
+  var idInfo = $(this).siblings(".description").attr("id");
+  //textareaArray= $(".description");
+  
+  console.log(idInfo);
+  textInfo = localStorage.setItem(idInfo, JSON.stringify(text));
   console.log("button clicked");
 };
 
 saveBtn.on("click", saveTasks);
+
 // WHEN I refresh the page
 // THEN the saved events persist
+var loadTask = function() {
+  var textareaArray= ["9", "10", "11","12", "13", "14", "15", "16","17"];
+  var currentTime = moment().hour();
+  for (var i = 0; i < textareaArray.length; i++) {
+    var id = "#" + textareaArray[i];
+    $(id).val(localStorage.getItem(textareaArray[i]));
+    //write within for loop 
+    if ( textareaArray[i] > currentTime) {
+      //$("p:first").addClass("intro");
+      $(id).addClass("bg-success");
+      //$("p").removeClass("intro");
+      $(id).removeClass("bg-primary");
+
+    } else if (textareaArray[i] < currentTime) {
+      $(id).addClass("bg-secondary");
+      $(id).removeClass("bg-primary");
+    }
+  }
+  
+};
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+loadTask();
